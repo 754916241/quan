4fb2eca08446fc50d7a932c14c623b0d933f6d4a
wyj <754916241@qq.com>
2018-02-28 21:36:22 +0800

    搭建大体结构

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CvMngComponent } from './recruit/cv-mng/cv-mng.component';
+import { JobMngComponent } from './recruit/job-mng/job-mng.component';
+import { JobPublishComponent } from './recruit/job-publish/job-publish.component';
+import { UserMngComponent } from './recruit/user-mng/user-mng.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvMngComponent,
+    JobMngComponent,
+    JobPublishComponent,
+    UserMngComponent
   ],
   imports: [
     BrowserModule,
